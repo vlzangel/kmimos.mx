53e812eaab75a7213a172abfe61c467f989a7231
Angel Veloz <vlzangel91@gmail.com>
2017-03-18 00:37:25 -0430

    No eliminaba las fotos de la galería

--- wp-content/themes/pointfinder/admin/estatemanagement/includes/pages/dashboard/dashboard-page.php
+++ wp-content/themes/pointfinder/admin/estatemanagement/includes/pages/dashboard/dashboard-page.php
@@ -689,17 +689,18 @@ if(isset($ua_action)){
 							try{
 	                         	if(isset($_GET['p'])){
 	                         		global $wpdb;
-
-	                         		echo $fullpath = "wp-content/uploads/cuidadores/galerias/".$user_id."/".$_GET['p'];
+                            		$cuidador = $wpdb->get_row("SELECT * FROM cuidadores WHERE user_id = {$user_id}");
+	                         		$tmp_user_id = ($cuidador->id) - 5000;
+	                         		$fullpath = "wp-content/uploads/cuidadores/galerias/".$tmp_user_id."/".$_GET['p'];
 	                         		if(file_exists($fullpath)){
 	                         			unlink($fullpath);
 	                         			echo '<h2>Imagen Eliminada Satisfactoriamente</h2>';
 	                         		}
 	                         	}
                          	}catch(Exception $e){}
-                 			/*echo '<br><a href="'.get_option( 'siteurl' ).'/perfil-usuario/?ua=mypictures">Mostrar Mis Fotos</a>"
+                 			echo '<br><a href="'.get_option( 'siteurl' ).'/perfil-usuario/?ua=mypictures">Mostrar Mis Fotos</a>"
                  			<br><small>Redireccionando a Mis Fotos</small>';
-                            echo '<script>location.href="'.get_option('siteurl').'/perfil-usuario/?ua=mypictures";</script>';*/
+                            echo '<script>location.href="'.get_option('siteurl').'/perfil-usuario/?ua=mypictures";</script>';
                         break;
 
                         case 'mypictures':
