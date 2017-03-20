4816c739b6387530ccfcb100cb653f2e01ced2a7
Angel Veloz <vlzangel91@gmail.com>
2017-03-19 22:34:13 -0430

    VLZ footer

---
+++ wp-content/themes/vlz/footer.php
@@ -0,0 +1,42 @@
+<footer class="kmimos_footer">            
+	<div class="container">
+	    <div class="row">
+	        <div class="col-md-4">
+	          	<h2>Contáctanos</h2>
+	          	<p>
+	            	<strong>Tlf:</strong> +52 (55) 1791.4931/ +52 (55) 66319264 <br>
+	            	<strong>Email:</strong> contactomex@kmimos.la
+           		</p>
+	        </div>
+	        <div class="col-md-4">
+	          	<h2>Navega</h2>
+	          	<ul>
+		            <li><a href="#">Nosotros</a></li>
+		            <li><a href="https://www.kmimos.com.mx/blog/">Blog</a></li>
+		            <li><a href="#">Preguntas y Respuestas</a></li>
+		            <li><a href="#">Cobertura Veterinaria</a></li>
+		            <li><a href="#">Comunicados de prensa</a></li>
+		            <li><a href="#">Términos y Condiciones</a></li>
+		            <li><a href="#">Nuestros Aliados</a></li>
+		            <li><a href="https://www.kmimos.com.mx/contacto/">Contáctanos</a></li>
+	          	</ul>
+	        </div>
+	    
+	        <div class="col-md-4">
+	          	<h2>¡B&uacute;scanos en nuestra redes sociales!</h2>
+	          	<div class="socialBtns">
+		            <a href="https://www.facebook.com/Kmimosmx" target="_blank" class="facebookBtn socialBtn" title="@kmimosmx"></a>
+		            <a href="https://www.twitter.com/Kmimosmx"  target="_blank" class="twitterBtn socialBtn" title="@kmimosmx"></a>
+		            <a href="https://www.instagram.com/kmimosmx/" target="_blank" class="instagramBtn socialBtn" title="@kmimosmx"></a>
+		            <img src="<?php echo get_template_directory_uri()."/recursos/imgs/dog.png"; ?>" alt="">
+	          	</div>
+	        </div>
+		</div> 
+	    <div class="row">
+		  	<div class="col-md-12">
+		    	<span id="siteseal"><script async type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=c5u9pjdoyKXQ6dRtmwnDmY0bV6KVBrdZGPEAnPkeSt7ZRCetPjIUzVK0bnHa"></script></span>   
+			</div>
+		</div> 
+  	</div>
+</footer>
+<?php //wp_footer(); ?>
