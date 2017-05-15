c8e9d61276eb157d9844d43c2c7aea27e20c1f4c
etpro01 <etpro@etprogroup.com>
2017-05-15 14:40:47 -0400

    whitelabel

--- volaris/index.php
+++ volaris/index.php
@@ -1,21 +1,12 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: kmimos
- * Date: 15-05-2017
- * Time: 02:19 PM
- */
-?>
-
-
+<!DOCTYPE HTML>
 <html>
     <head>
         <style type="text/css">
             html, body{ position: relative; height: 100%;  margin: 0; padding: 0;}
-            iframe{ position: absolute; width: 100%; height: 100%;}
+            iframe{ position: absolute; width: 100%; height: 100%; border: none;}
         </style>
     </head>
     <body>
-        <iframe src="https://www.kmimos.com.mx/?wlabel=volaris" width="100%"/>
+        <iframe src="https://www.kmimos.com.mx/?wlabel=volaris"/>
     </body>
 </html>
