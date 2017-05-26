f798346309b4c6008a88f29375c692f3542f0f91
Angel Veloz <vlzangel91@gmail.com>
2017-05-26 19:21:19 +0200

    Bug cancelacion

--- wp-content/plugins/kmimos/includes/functions/vlz_functions.php
+++ wp-content/plugins/kmimos/includes/functions/vlz_functions.php
@@ -68,7 +68,10 @@
             $descuento = 0;
             if( $metas_orden[ "_cart_discount" ][0] != "" ){
                 $descuento = $metas_orden[ "_cart_discount" ][0]+0;
+                $saldo += $descuento;
             }
+
+            echo "Saldo: ".$saldo;
 
             $saldo_persistente = get_user_meta($id_cliente, "kmisaldo", true)+0;
             update_user_meta($id_cliente, "kmisaldo", $saldo_persistente+$saldo);
