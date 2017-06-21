458470a43f69b1e59f1ac05ec897992831323afb
etpro01 <etpro@etprogroup.com>
2017-06-21 11:28:30 -0400

    wlabel

--- wp-content/plugins/kmimos/wlabel/index.php
+++ wp-content/plugins/kmimos/wlabel/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-        <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+        <?php //<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>  ?>
         <script type="text/javascript" src="<?php echo plugin_dir_url( __FILE__ ); ?>includes/js/script.js"></script>
         <link media="all" type="text/css" rel="stylesheet" href="<?php echo plugin_dir_url( __FILE__ ); ?>includes/css/style.css"/>
         <title>PANEL WHITE LABEL</title>
