929e85dbeed0ee07be1885b7e753e489c1463317
www-data <www-data@ip-172-31-5-225.us-west-2.compute.internal>
2017-07-10 21:31:02 +0000

    Cambios desde QA

--- wp-content/themes/pointfinder/functions.php
+++ wp-content/themes/pointfinder/functions.php
@@ -37,20 +37,20 @@
 	    remove_action( 'wp_head', 'wp_print_scripts' );
 	    remove_action( 'wp_head', 'wp_print_head_scripts', 9 );
 	    remove_action( 'wp_head', 'wp_enqueue_scripts', 1 );
-	 
 	    add_action( 'wp_footer', 'wp_print_scripts', 5);
 	    add_action( 'wp_footer', 'wp_enqueue_scripts', 5);
 	    add_action( 'wp_footer', 'wp_print_head_scripts', 5);
 	}
 	add_action('wp_enqueue_scripts', 'move_scripts_from_head_to_footer');
 
-
+/*
 	function _remove_script_version( $src ){
 	    $parts = explode( '?ver', $src );
         return $parts[0]."?ver=".time();
 	}
 	add_filter( 'script_loader_src', '_remove_script_version', 15, 1 );
 	add_filter( 'style_loader_src', '_remove_script_version', 15, 1 );
+*/
 
 	add_filter( 'woocommerce_product_tabs', 'sb_woo_remove_reviews_tab', 98);
 	function sb_woo_remove_reviews_tab($tabs) {
