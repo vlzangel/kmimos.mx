38b87a8f8c16c78037d2a46750e4fc99c74c6d27
Dajan Medina <dmedina2150@gmail.com>
2017-09-08 21:04:51 -0400

    Actualizacion de modales registro y conoce al cuidador

--- wp-content/themes/kmimos/page-busqueda.php
+++ wp-content/themes/kmimos/page-busqueda.php
@@ -193,7 +193,7 @@
 			<script type="text/javascript" src="'.getTema().'/js/markerclusterer.js"></script>
 			<script type="text/javascript" src="'.getTema().'/js/oms.min.js"></script>
     ';
-
+    include("partes/modal_conocelo.php");
 	echo comprimir_styles($HTML);
 
 
