66c7b3349aadd6e4060d8614c57fb1a0e1263bf7
Dajan Medina <dmedina2150@gmail.com>
2017-10-05 16:21:26 -0400

    Agregando pruebas para mailing

---
+++ landing/mailing.php
@@ -0,0 +1,23 @@
+<?php
+	header('Access-Control-Allow-Origin: *');
+	// Wordpress
+    define('WP_USE_THEMES', false);
+
+    require('../wp-blog-header.php');
+    //date_default_timezone_set('America/Mexico_City');
+    global $wpdb;
+	
+	$http = (isset($_SERVER['HTTPS']))? 'https://' : 'http://' ;
+	 extract($_POST);
+	$email = $_POST['email'];
+    # ********************************************
+    # Email de Nuevo registro participantes 
+    # ********************************************
+	require_once('email_template/email_mailing.php');
+	wp_mail(
+		$email,
+		"Lista Mailing",
+		$html
+	);
+    # ********************************************
+    echo "<scritp>alert('Enviado email a '+".$email.");</scritp>";
---
+++ prueba_email/js/main.js
@@ -0,0 +1,53 @@
+
+$('#frm').on('submit', function(e){
+  e.preventDefault(e);
+  _registerLanding();
+});
+
+$('#send').on('click', function(e){
+  _registerLanding();
+});
+
+$("input").change(function(){
+  if( $(this).val() != "" ){
+    $('msg').html("");
+  }
+});
+
+function _registerLanding(){
+
+  if( $('#email').val() == "" || $('#name').val() == "" ){
+    $('#msg').html('Debe completar los datos');
+    return;
+  }
+
+  $('#loading').removeClass('hidden');
+  $('#msg').html('Registrando Usuario.');
+  $.ajax( url+"/landing/registro-usuario.php?email="+$('#email').val()+"&name="+$('#name').val()+"&referencia="+$('#referencia').val() )
+  .done(function() {
+    $('#msg').html('Generando url.');
+  })
+  .fail(function() {
+    $('#msg').html('Registro: No pudimos completar su solicitud, intente nuevamente');
+    $('#loading').addClass('hidden');
+  });  
+
+
+
+  $('#loading').removeClass('hidden');
+  $('#msg').html('Enviando...');
+  $.ajax( url+"/landing/list-subscriber.php?source=kmimos-mx-clientes-referidos&email="+$('#email').val() )
+  .done(function() {
+    $('#loading').addClass('hidden');
+    $('#msg').html('Guardando referencia.');
+
+    //window.open($('#temp').attr('action')+$('#email').val(), '_system');
+    window.location.href = $('#temp').attr('action')+$('#email').val();
+  })
+  .fail(function() {
+    //$('#msg').html('Referencia: No pudimos completar su solicitud, intente nuevamente');
+    //$('#loading').addClass('hidden');
+  });  
+
+}
+
