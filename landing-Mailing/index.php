114b2ae282eefd9a3e496b031c1e68fc27b88014
Dajan Medina <dmedina2150@gmail.com>
2017-10-06 13:20:54 -0400

    Correccion html mailing 2.4

--- landing-Mailing/index.php
+++ landing-Mailing/index.php
@@ -34,7 +34,12 @@
 				</div>
 				<div style="width: 50%; float: left;">
 					<div style="background: #9c2a81!important;width: 100%; height: 228px;margin-top: -8%; position: relative;">
-	    				<p style="font-size: 15pt;color: #fff;text-align: right;top: 5%; position: relative;right: 5%;">Viaja tranquilo <br> deja a tu perro seguro <br> en el hogar de una verdadera familia</p>
+	    				<p style="font-size: 15pt;
+	    				color: #fff;
+	    				text-align: right;
+	    				margin-top: 8%; 
+	    				position: relative;
+	    				right: 5%;">Viaja tranquilo <br> deja a tu perro seguro <br> en el hogar de una verdadera familia</p>
 	    				<!-- <img src="https://kmimos.com.mx/landing/img/Button-2.jpg" alt="boton 1" class="btn-reserva"> -->
 	    				<a href="https://www.kmimos.com.mx/?wlabel=volaris"><img src="http://kmimosmx.sytes.net/QA1/prueba_email/img/Button-2.jpg" alt="boton 1" style="width: 55%; position: relative; left: 35%;margin-top: 5%;"></a>
 	    			</div>
