efaf9eaa02a0d85d71994e25aaa206137e3773fa
Dajan Medina <dmedina2150@gmail.com>
2017-10-17 12:41:50 -0400

    Registro subscribers landing guarderia-perro

--- guarderia-perro/index.php
+++ guarderia-perro/index.php
@@ -7,6 +7,7 @@
     <meta name="keywords" content="">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <style title="page-styles" type="text/css" data-page-type="main_desktop">
         body { color:#000; }
         a {
@@ -2263,7 +2264,8 @@
     <link href="//builder-assets.unbounce.com/published/page-defaults-87d8ba1.z.css" rel="stylesheet" media="screen" type="text/css">
 
     <meta property="og:title" content="http://www.kmimos.com.mx/guarderia-perro/">
-    <script type="text/javascript" async="" src="//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js"></script><script src="//ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" type="text/javascript"></script>
+    <!-- <script type="text/javascript" async="" src="//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js"></script> -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" type="text/javascript"></script>
     <script type="text/javascript" src="//d2xxq4ijfwetlm.cloudfront.net/m/lp-webapp/api/unbounce.js"></script>
     <script type="text/javascript">
         window.ub.page.id = "d528eae2-fd0d-11e6-a134-02425fb9cdd7";
@@ -2277,7 +2279,8 @@
         var lp = lp || {};
         lp.jQuery = jQuery.noConflict( true );
     </script>
-
+    <script src="jquery/jquery.js"></script>
+        
     <script src="//builder-assets.unbounce.com/modules/lp-block/public/main-90cd9cc.z.js" type="text/javascript"></script>
     <script src="//builder-assets.unbounce.com/published/matchMedia-ba7af92.z.js" type="text/javascript"></script>
     <script src="//builder-assets.unbounce.com/modules/lp-text/public/main-428abbf.z.js" type="text/javascript"></script>
@@ -2389,8 +2392,6 @@
         }
 
     </style>
-
-
     <style>
         @import url(https://www.onlinewebfonts.com/db.onlinewebfonts.com/c/9ecf943eacd910490a17d5ca5af92641%3Ffamily=CanaroW00-Light);
 
@@ -2402,14 +2403,13 @@
             url("https://www.onlinewebfonts.com/db.onlinewebfonts.com/t/9ecf943eacd910490a17d5ca5af92641.ttf") format("truetype"),
             url("https://www.onlinewebfonts.com/db.onlinewebfonts.com/t/9ecf943eacd910490a17d5ca5af92641.svg#CanaroW00-Light") format("svg");
         }
-
     </style>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" type="text/javascript"></script>
 
-    <!-- start Mixpanel --><script type="text/javascript">(function(e,a){if(!a.__SV){var b=window;try{var c,l,i,j=b.location,g=j.hash;c=function(a,b){return(l=a.match(RegExp(b+"=([^&]*)")))?l[1]:null};g&&c(g,"state")&&(i=JSON.parse(decodeURIComponent(c(g,"state"))),"mpeditor"===i.action&&(b.sessionStorage.setItem("_mpcehash",g),history.replaceState(i.desiredHash||"",e.title,j.pathname+j.search)))}catch(m){}var k,h;window.mixpanel=a;a._i=[];a.init=function(b,c,f){function e(b,a){var c=a.split(".");2==c.length&&(b=b[c[0]],a=c[1]);b[a]=function(){b.push([a].concat(Array.prototype.slice.call(arguments,
+    <!-- start Mixpanel <script type="text/javascript">(function(e,a){if(!a.__SV){var b=window;try{var c,l,i,j=b.location,g=j.hash;c=function(a,b){return(l=a.match(RegExp(b+"=([^&]*)")))?l[1]:null};g&&c(g,"state")&&(i=JSON.parse(decodeURIComponent(c(g,"state"))),"mpeditor"===i.action&&(b.sessionStorage.setItem("_mpcehash",g),history.replaceState(i.desiredHash||"",e.title,j.pathname+j.search)))}catch(m){}var k,h;window.mixpanel=a;a._i=[];a.init=function(b,c,f){function e(b,a){var c=a.split(".");2==c.length&&(b=b[c[0]],a=c[1]);b[a]=function(){b.push([a].concat(Array.prototype.slice.call(arguments,
             0)))}}var d=a;"undefined"!==typeof f?d=a[f]=[]:f="mixpanel";d.people=d.people||[];d.toString=function(b){var a="mixpanel";"mixpanel"!==f&&(a+="."+f);b||(a+=" (stub)");return a};d.people.toString=function(){return d.toString(1)+".people (stub)"};k="disable time_event track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config reset people.set people.set_once people.increment people.append people.union people.track_charge people.clear_charges people.delete_user".split(" ");
             for(h=0;h<k.length;h++)e(d,k[h]);a._i.push([b,c,f])};a.__SV=1.2;b=e.createElement("script");b.type="text/javascript";b.async=!0;b.src="undefined"!==typeof MIXPANEL_CUSTOM_LIB_URL?MIXPANEL_CUSTOM_LIB_URL:"file:"===e.location.protocol&&"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\/\//)?"https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js":"//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";c=e.getElementsByTagName("script")[0];c.parentNode.insertBefore(b,c)}})(document,window.mixpanel||[]);
-        mixpanel.init("972817bb3a7c91a4b95c1641495dfeb7");</script><!-- end Mixpanel -->
+        mixpanel.init("972817bb3a7c91a4b95c1641495dfeb7");</script> end Mixpanel  -->
 
     <script src="//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,400italic,700,700italic%7CMontserrat:400,400italic,700,700italic%7CLato:400,400italic,700,700italic"><script>
@@ -2464,7 +2464,152 @@
 
     <!-- lp:insertions end head -->
 
-    <style type="text/css">@media only screen and (max-width: 480px), only screen and (max-device-width: 480px) {* {-webkit-text-size-adjust: 98%;}}</style></head>
+    <style type="text/css">@media only screen and (max-width: 480px), only screen and (max-device-width: 480px) {* {-webkit-text-size-adjust: 98%;}}</style>
+    <style type="text/css">
+        
+        #message{position: fixed; width: 100%; height: 100%; bottom: 0; padding: 20px; text-align: center; box-shadow: 0 0 3px #CCC; background: rgba(0, 0, 0, 0.8); z-index: 2;}
+        #message.Msubscribe .contain{position: relative; width: 95%; max-width: 100%; margin: 0 auto;}
+        #PageSubscribe{position:relative; max-width: 700px;  margin: 0 auto;  padding: 25px;  top: 75px; color: #FFF; border-radius: 20px; /* background:#00bc00;*/
+            background: #ba2287;  overflow: hidden;}
+        #PageSubscribe .exit{float: right; cursor: pointer;}
+        #PageSubscribe .section{ width: 45%; padding: 10px; float: left; font-size: 17px; text-align: left;}
+        #PageSubscribe .section.section1{font-size: 20px;}
+        #PageSubscribe .section.section1 span{font-size: 25px; font-weight: 400;}
+        #PageSubscribe .section.section1 .images{padding:10px 0; text-align: center;}
+        #PageSubscribe .section.section3{width: 100%; font-size: 17px; font-weight: bold; text-align: center;}
+        #PageSubscribe .section.section2{}
+        #PageSubscribe .section.section2 .message{font-size: 15px; border: none; background: none; opacity:0; visibility: : hidden; transition: all .3s;}
+        #PageSubscribe .section.section2 .message.show{opacity:1; visibility: :visible;}
+        #PageSubscribe .section.section2 .icon{width: 30px; padding: 5px 0;}
+        #PageSubscribe .section.section2 .subscribe {margin: 20px 0;  }
+        #PageSubscribe .section.section2 form{margin: 0; display:flex;}
+        #PageSubscribe .section.section2 input,
+        #PageSubscribe .section.section2 button{width: 100%; max-width: calc(100% - 60px); margin: 5px; padding: 5px 10px; color: #CCC; font-size: 15px; border-radius: 20px;  border: none; background: #FFF; }
+        #PageSubscribe .section.section2 button {padding: 10px;  width: 29px;}
+
+        @media screen and (max-width:480px), screen and (max-device-width:480px) {
+            #PageSubscribe { top: 15px;}
+            #PageSubscribe .section{ width: 100%; padding: 10px 0; font-size: 12px;}
+            #PageSubscribe .section.section1 {font-size: 15px;}
+            #PageSubscribe .section.section1 span {font-size: 20px;}
+            #PageSubscribe .section.section3 {font-size: 12px;}
+            #PageSubscribe .section.section2 input, #PageSubscribe .section.section2 button {font-size: 10px;}
+        }
+    </style>
+
+    <script type='text/javascript'>
+        //Subscribe
+        function SubscribeSite(){
+            clearTimeout(SubscribeTime);
+
+            var dog = '<img height="70" align="bottom" src="https://www.kmimos.com.mx/wp-content/uploads/2017/07/propuestas-banner-09.png">' +
+                '<img height="20" align="bottom" src="https://www.kmimos.com.mx/wp-content/uploads/2017/07/propuestas-banner-10.png">';
+
+            var html='<div id="PageSubscribe"><i class="exit fa fa-times" aria-hidden="true" onclick="SubscribePopUp_Close(\'#message.Msubscribe\')"></i>' +
+                '<div class="section section1"><span>G&aacute;nate <strong>$50 pesos</strong> en tu primera reserva</span><br>&#8216;&#8216;Aplica para clientes nuevos&#8217;&#8217;<div class="images">'+dog+'</div></div>' +
+                '<div class="section section2"><span><strong>&#161;SUSCR&Iacute;BETE!</strong> y recibe el Newsletter con nuestras <strong>PROMOCIONES, TIPS DE CUIDADOS PARA MASCOTAS,</strong> etc.!</span>'+
+
+                '<div class="subscribe">'+
+                '<form onsubmit="form_subscribe(this); return false;">'+
+                '<input type="hidden" name="section" value="landing-volaris"/>'+
+                '<input type="mail" name="mail" value="" placeholder="Introduce tu correo aqu&iacute" required/>'+
+                '<button type="submit"><i class="fa fa-arrow-right" aria-hidden="true"></i></button>'+
+                '</form>'+
+                '<div class="message"></div>'+
+                '</div>'+
+
+                '</div>'+
+                '<div class="section section3">*Dentro de 48 hrs. Te enviaremos v&iacute;a email tu c&uacute;pon de descuento</div>' +
+                '</div>';
+            SubscribePopUp_Create(html);
+        }
+
+        function SubscribePopUp_Create(html){
+            var element = '#message.Msubscribe';
+            if(jQuery(element).length==0){
+                jQuery('body').append('<div id="message" class="Msubscribe"></div>');
+                jQuery(element).append('<div class="contain"></div>');
+            }
+
+            jQuery(element).find('.contain').html(html);
+            jQuery(element).fadeIn(500,function(){
+                /*
+                 vsetTime = setTimeout(function(){
+                 SubscribePopUp_Close(element);
+                 }, 6000);
+                 */
+            });
+        }
+
+        jQuery(document).ready(function(e){
+            SubscribeTime = setTimeout(function(){
+                SubscribeSite();
+            }, 7400);
+        });
+
+        function form_subscribe(element){
+            var subscribe = jQuery(element).closest('.subscribe');
+            var message = subscribe.find('.message');
+            var email = subscribe.find('input[name="mail"]').val();
+            var url = '../landing/newsletter.php?source=guarderia-perro&email='+email;
+            if(email!=''){
+                jQuery.post(url, jQuery(element).serialize(),function(data){
+                    //console.log(data);
+                    var textmessage="Error al guardar los datos";
+
+                    if( data == 1){
+                        textmessage="Datos guardados";
+                        var datos = {'campo':'cm-vydldy-vydldy',
+                                     'email': email,
+                                     'lista': 'http://kmimos.intaface.com/t/j/s/vydldy/'}
+                        var resp = getGlobalData('https://www.kmimos.com.mx/landing-volaris/suscribir_lista.php','POST', datos);
+                    }else if( data == 2){
+                        textmessage="Formato de email invalido";
+                    }else if( data == 3){
+                        textmessage="Ya est&aacute;s registrado en la lista, Gracias!";
+                    }else{
+                        textmessage="Error al guardar los datos";
+                    }
+
+                    if(message.length>0){
+                        message.addClass('show');
+                        message.html('<i class="icon fa fa-envelope"></i>'+textmessage+'');
+                        vsetTime = setTimeout(function(){
+                            message_subscribe(message);
+                        }, 5000);
+                    }
+                });
+            }
+            return false;
+        }
+
+        function message_subscribe(element){
+            clearTimeout(vsetTime);
+            element.removeClass('show');
+            element.html('');
+            return true;
+        }
+
+        function SubscribePopUp_Close(element){
+            if(jQuery(element).length>0){
+                jQuery(element).fadeOut(500,function(){
+                    jQuery(element).remove();
+                });
+            }
+        }
+        function getGlobalData(url,method, datos){
+            return jQuery.ajax({
+                data: datos,
+                type: method,
+                url: url,
+                async:false,
+                success: function(data){
+                    return data;
+                }
+            }).responseText;
+        }
+    </script>
+    </head>
 <body class="lp-pom-body ">
 <!-- lp:insertions start body:before -->
 <!-- lp:insertions end body:before -->
@@ -2767,9 +2912,9 @@
         });
     });
 </script>
-<script>
+<!-- <script>
     mixpanel.track("LandingGooglePageView");
-</script>
+</script> -->
 <script type="text/javascript">// See README.md for documentation
     lp.jQuery().ready(function($) {
         $('input[name^=ubafs-]', 'form').remove();
